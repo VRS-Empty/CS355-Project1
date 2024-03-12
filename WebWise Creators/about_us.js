8350477f16f6986bb1311e28041aae9cc6e0a17c
galib4444 <163075514+galib4444@users.noreply.github.com>
2024-03-12 00:36:37 -0400

    Add files via upload

---
+++ WebWise Creators/about_us.js
@@ -0,0 +1,9 @@
+
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    var moreInfo = document.getElementById('moreInfo');
+    if (moreInfo.style.display === 'none') {
+        moreInfo.style.display = 'block';
+    } else {
+        moreInfo.style.display = 'none';
+    }
+});
